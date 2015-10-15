a497d3745f76c30df019df139c95b3b61c388071
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-10-15 16:31:20 +0300

    replace " with '

--- scraper.js
+++ scraper.js
@@ -1,29 +1,29 @@
 // This is a template for a Node.js scraper on morph.io (https://morph.io)
 
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+var cheerio = require('cheerio');
+var request = require('request');
+var sqlite3 = require('sqlite3').verbose();
 
 function initDatabase(callback) {
   // Set up sqlite database.
-  var db = new sqlite3.Database("data.sqlite");
+  var db = new sqlite3.Database('data.sqlite');
   db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+    db.run('CREATE TABLE IF NOT EXISTS data (name TEXT)');
     callback(db);
   });
 }
 
 function updateRow(db, value) {
   // Insert some data.
-  var statement = db.prepare("INSERT INTO data VALUES (?)");
+  var statement = db.prepare('INSERT INTO data VALUES (?)');
   statement.run(value);
   statement.finalize();
 }
 
 function readRows(db) {
   // Read some data.
-  db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-    console.log(row.id + ": " + row.name);
+  db.each('SELECT rowid AS id, name FROM data', function(err, row) {
+    console.log(row.id + ': ' + row.name);
   });
 }
 
@@ -31,7 +31,7 @@ function fetchPage(url, callback) {
   // Use request to read in pages.
   request(url, function (error, response, body) {
     if (error) {
-      console.log("Error requesting page: " + error);
+      console.log('Error requesting page: ' + error);
       return;
     }
 
@@ -41,11 +41,11 @@ function fetchPage(url, callback) {
 
 function run(db) {
   // Use request to read in pages.
-  fetchPage("https://morph.io", function (body) {
+  fetchPage('https://morph.io', function (body) {
     // Use cheerio to find things in the page with css selectors.
     var $ = cheerio.load(body);
 
-    var elements = $("div.media-body span.p-name").each(function () {
+    var elements = $('div.media-body span.p-name').each(function () {
       var value = $(this).text().trim();
       updateRow(db, value);
     });
