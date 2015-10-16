0212fe2edb8b9c134636cb615e5a1a5578aab6d7
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-10-17 02:14:53 +0300

    Initial version. Just URL and title for now.

--- scraper.js
+++ scraper.js
@@ -1,58 +1,90 @@
 // This is a template for a Node.js scraper on morph.io (https://morph.io)
 
+var assert = require('assert');
+var _ = require('lodash');
 var cheerio = require('cheerio');
+var async = require('async');
 var request = require('request');
 var sqlite3 = require('sqlite3').verbose();
 
+var db = new sqlite3.Database('data.sqlite');
+
 function initDatabase(callback) {
   // Set up sqlite database.
-  var db = new sqlite3.Database('data.sqlite');
   db.serialize(function() {
-    db.run('CREATE TABLE IF NOT EXISTS data (name TEXT)');
+    db.run('CREATE TABLE IF NOT EXISTS apis (url TEXT, data TEXT, PRIMARY KEY(url))');
     callback(db);
   });
 }
 
-function updateRow(db, value) {
+function updateRow(url, value) {
   // Insert some data.
-  var statement = db.prepare('INSERT INTO data VALUES (?)');
-  statement.run(value);
+  var statement = db.prepare('REPLACE INTO apis VALUES ($url, $data)');
+  statement.run({$url: url, $data: value});
   statement.finalize();
 }
 
-function readRows(db) {
+function readRows() {
   // Read some data.
-  db.each('SELECT rowid AS id, name FROM data', function(err, row) {
-    console.log(row.id + ': ' + row.name);
+  db.each('SELECT url, data FROM apis', function(err, row) {
+    console.log(row.url + ': ' + row.data);
   });
 }
 
 function fetchPage(url, callback) {
-  // Use request to read in pages.
+  url = 'http://www.programmableweb.com' + url;
   request(url, function (error, response, body) {
-    if (error) {
-      console.log('Error requesting page: ' + error);
-      return;
-    }
+    assert(!error, 'Error requesting ' + url + ': ' + error);
+    assert(response.statusCode === 200, 'Can not GET "' + url +'": ' + response.statusMessage);
+    console.log(url);
+    callback(cheerio.load(body));
+  });
+}
 
-    callback(body);
+function directoryPage(url, links, callback) {
+  fetchPage(url, function ($) {
+    $('.col-md-3 a').each(function () {
+      links.push($(this).attr('href'));
+    });
+    var next = $('.pager-last a').attr('href');
+    if (next)
+      return directoryPage(next, links, callback);
+    callback(links);
+  });
+}
+
+function getLinks(callback) {
+  directoryPage('/apis/directory', [], function (links) {
+    callback(links);
+  });
+}
+
+function apiPage(url, callback) {
+  fetchPage(url, function ($) {
+    var row = [url, $('.node-header h1').text()];
+    //console.log($('.node-header h1').text());
+    //console.log($('.api_description').text().trim());
+    //$('.specs .field').each(function () {
+    //  console.log($(this).children('label').text());
+    //  console.log($(this).children('span').text());
+    //});
+    //console.log($('.followers-block .block-title span').text());
+    callback(row);
   });
 }
 
 function run(db) {
-  // Use request to read in pages.
-  fetchPage('https://morph.io', function (body) {
-    // Use cheerio to find things in the page with css selectors.
-    var $ = cheerio.load(body);
-
-    var elements = $('div.media-body span.p-name').each(function () {
-      var value = $(this).text().trim();
-      updateRow(db, value);
+  getLinks(function (links) {
+    async.forEachOfSeries(links, function (url, index, asyncCb) {
+      apiPage(url, function (row) {
+        updateRow(row[0], row[1]);
+        asyncCb(null);
+      });
+    }, function () {
+      console.log('Finish');
+      readRows(db);
+      db.close();
     });
-
-    readRows(db);
-
-    db.close();
   });
 }
 
